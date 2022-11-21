0de337d383974f83a124b1f82dd42fc642566c3d
Aleksandar <a.milenkovic321@gmail.com>
2022-11-21 13:29:08 +0100

    Some updates

--- public/js/level.js
+++ public/js/level.js
@@ -2,12 +2,10 @@ import { isGameStart } from "./game-started.js";
 
 export const linkName = (link, setGameStart, gameStart) => {
     setGameStart(true);
-    
     isGameStart(gameStart);
     const onConfirmRefresh = function (event) {
         event.preventDefault();
         return event.returnValue = "Attention";
     }
-      
     window.addEventListener("beforeunload", onConfirmRefresh, { capture: true });
 }
