283a772bf8109ac7946c5939c017c1a12004f28d
Aleksandar16 <a.milenkovic321@gmail.com>
2022-11-26 19:25:42 +0100

    generate table

--- public/js/game-started.js
+++ public/js/game-started.js
@@ -1,14 +1,18 @@
-export const gameStart = () => {
-    document.querySelector(".container").classList.remove("hide");
+import { generateTable, deleteTable } from "./table.js";
+
+export const gameStart = (difficulty) => {
+    generateTable(difficulty);
     document.querySelector("h2").classList.add("hide");
     document.querySelectorAll("li").forEach(function(el) {
         el.classList.add("hide");
     });
+    document.querySelector("#btn").classList.remove("hide");
 }
 export const gameStop = () => {
-    document.querySelector(".container").classList.add("hide");
+    deleteTable();
     document.querySelector("h2").classList.remove("hide");
     document.querySelectorAll("li").forEach(function(el) {
         el.classList.remove("hide");
     });
+    document.querySelector("#btn").classList.add("hide");
 }
--- public/js/level.js
+++ public/js/level.js
@@ -1,11 +1,11 @@
 import { gameStart } from "./game-started.js";
 
 export const linkName = (difficulty) => {
-  gameStart();
+  console.log(difficulty);
+  gameStart(difficulty);
   const onConfirmRefresh = function (event) {
     event.preventDefault();
     return (event.returnValue = "Attention");
   };
   window.addEventListener("beforeunload", onConfirmRefresh, { capture: true });
-  document.querySelector("#case1").setAttribute('autofocus', 'autofocus');
 };
